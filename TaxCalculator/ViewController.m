cec3304d165c5cd898ecec9e7a256e88031abe87
andrewbihl <andrewbihlva@gmail.com>
2016-04-22 15:24:46 -0400

    first commit

--- TaxCalculator/ViewController.m
+++ TaxCalculator/ViewController.m
@@ -9,6 +9,15 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *priceTextField;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *segmentedControl;
+- (IBAction)onCalculateButtonTapped:(UIButton *)sender;
+@property (weak, nonatomic) IBOutlet UILabel *resultLabel;
+//@property NSDictionary* cityTaxes;
+@property NSArray* cityTaxes;
+//@property double sfTax;
+//@property double chiTax;
+//@property double nyTax;
 
 @end
 
@@ -16,12 +25,33 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    _cityTaxes = @[@8.75, @10.25, @8.875];
+//    _sfTax = 8.75;
+//    _chiTax = 10.25;
+//    _nyTax = 8.875;
     // Do any additional setup after loading the view, typically from a nib.
 }
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)onCalculateButtonTapped:(UIButton *)sender {
+    int segmentSelection = _segmentedControl.selectedSegmentIndex;
+    NSNumber* taxRate = [_cityTaxes objectAtIndex:segmentSelection];
+    //NSLog(@"%i",_segmentedControl.selectedSegmentIndex);
+    float result = (double)[_priceTextField.text intValue];
+    result *= ([taxRate floatValue] / 100);
+    NSString* resultString = [NSString stringWithFormat: @"%f",result];
+    for (int i = resultString.length-1; i>2; i--)
+        if ([resultString characterAtIndex:i] == '0')
+            resultString = [resultString substringToIndex: i];
+        else if ([resultString characterAtIndex:i] == '.')
+            resultString = [NSString stringWithFormat: @"%@%@",resultString,@"0"];
+        else
+            break;
+    _resultLabel.text = resultString;
+}
 @end
